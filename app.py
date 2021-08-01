52696ce7e1a362e9b048fab2d3b2786a61f7aa41
Ben Schmaltz <beschmal@microsoft.com>
2021-07-31 18:09:22 -0700

    setup token validation code

--- app.py
+++ app.py
@@ -1,65 +1,36 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
-
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
+    return render_template('index.html')
 
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
+@app.route('/validate', methods=['POST'])
+def validate():
+    # Send request to MISE container
+    # TODO: actually send request and use response
+    url = "http://localhost:8080/validatetoken"
+    token = request.form["token"]
+    headers = {}
+    data = {}
+    # r = requests.post(url = url, headers = headers, data = data)
+    # if r.status_code == 200:
 
+    # return response based on response from validator
+    if True:
+        return jsonify({
+            "success": True,
+            "inputToken": token
+        })
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
-
-
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting that task'
-
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-
-    if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-
-    else:
-        return render_template('update.html', task=task)
-
+        return jsonify({
+            "success": False,
+            "inputToken": token
+        })
 
 if __name__ == "__main__":
     app.run(debug=True)
